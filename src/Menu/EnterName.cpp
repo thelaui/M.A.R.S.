a98447be22ba6cba60107eae52bec504ee180e31
m-c-t <m-c-t@249382d3-5f29-4b26-bf2c-91a77bf04246>
2011-02-27 09:53:55 +0000

    added translations, made the label in the enter-name-window a textbox
    git-svn-id: https://svn.code.sf.net/p/mars-game/code@148 249382d3-5f29-4b26-bf2c-91a77bf04246

--- src/Menu/EnterName.cpp
+++ src/Menu/EnterName.cpp
@@ -22,6 +22,7 @@ this program.  If not, see <http://www.gnu.org/licenses/>. */
 # include "Interface/Checkbox.hpp"
 # include "Menu/menus.hpp"
 # include "Interface/TextEdit.hpp"
+# include "Interface/TextBox.hpp"
 # include "Interface/Line.hpp"
 # include "System/settings.hpp"
 # include "Media/text.hpp"
@@ -32,13 +33,13 @@ bool EnterName::kOk_(false);
 
 UiWindow* EnterName::get() {
     if (instance_ == NULL) {
-        instance_ = new EnterName(320, 150);
-        instance_->addWidget(new TextEdit(NULL, &settings::C_playerIName, Vector2f(10,65), 300, 0, TEXT_EDIT, 12));
-        instance_->addWidget(new Button(locales::getLocale(locales::Ok), NULL, &kOk_, Vector2f(220,120), 90, 20));
+        instance_ = new EnterName(320, 160);
+        instance_->addWidget(new TextEdit(NULL, &settings::C_playerIName, Vector2f(10,85), 300, 0, TEXT_EDIT, 12));
+        instance_->addWidget(new Button(locales::getLocale(locales::Ok), NULL, &kOk_, Vector2f(220,130), 90, 20));
         instance_->addWidget(new Label(locales::getLocale(locales::PleaseEnterName), TEXT_ALIGN_LEFT, Vector2f(10,10), 20.f, Color3f(1.f, 0.5f, 0.9f), false));
         instance_->addWidget(new Label(locales::getLocale(locales::Info), TEXT_ALIGN_RIGHT, Vector2f(310,18), 12.f, Color3f(1.f, 0.5f, 0.9f), false));
         instance_->addWidget(new Line(Vector2f(10, 35), Vector2f(310, 35)));
-        instance_->addWidget(new Label(locales::getLocale(locales::PleaseEnterNameText), TEXT_ALIGN_LEFT, Vector2f(10, 40)));
+        instance_->addWidget(new TextBox(locales::getLocale(locales::PleaseEnterNameText), Vector2f(10, 40), 300, 30));
     }
     return instance_;
 }
