437d342c6958a3a7bdc38d9ed255c400e025c78d
m-c-t <m-c-t@249382d3-5f29-4b26-bf2c-91a77bf04246>
2011-03-10 13:53:01 +0000

    updated credits
    git-svn-id: https://svn.code.sf.net/p/mars-game/code@174 249382d3-5f29-4b26-bf2c-91a77bf04246

--- include/defines.hpp
+++ include/defines.hpp
@@ -21,7 +21,7 @@ this program.  If not, see <http://www.gnu.org/licenses/>. */
 # define VERSION_MAJOR               "0"
 # define VERSION_MINOR               "7"
 # define VERSION_PATCH               "0"
-# define VERSION_REVIS               "171"
+# define VERSION_REVIS               "175"
 
 # define SHIP_GRAPHICS_COUNT         14
 
